9886b48968ebae70cfdb811830149184af02a5e4
Chenyi Wang <cyeeeew@gmail.com>
2023-10-12 18:31:32 -0400

    Week 5 initial commit

---
+++ Week5/sketch.js
@@ -0,0 +1,15 @@
+/*
+Week 5 - Data
+
+Instruction:
+Create a sketch that calls an API, processes the data, and visualizes it somehow.
+
+*/
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
