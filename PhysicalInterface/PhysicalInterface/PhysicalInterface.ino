acca9a1007dab3b6d7ee30504cf0e85a8ad4d78f
Chenyi Wang <cyeeeew@gmail.com>
2023-11-26 17:01:50 -0500

    Implemented physical interface

--- PhysicalInterface/PhysicalInterface/PhysicalInterface.ino
+++ PhysicalInterface/PhysicalInterface/PhysicalInterface.ino
@@ -1,9 +1,121 @@
-void setup() {
-  // put your setup code here, to run once:
+/*
+Physical Interface
+Author: Chenyi Wang
+Date: 11/26/2023
 
+Prompt: Build a sketch with a physical interface - take one of your early p5 sketches and make it controllable with digital or analog inputs using serial.
+
+Iteration on tic-tac-toe game - switch the game board to a p5 sketch interface while keeping the player controls physical
+*/
+
+/* pins */
+const int DIAL = A0;            // input pin for potentiometer
+const int BTN_1 = 2;            // input pin for pushbutton 1
+const int BTN_2 = 4;            // input pin for pushbutton 2
+
+/* potentiometer values */
+int dialCurrValue = 0;
+int dialMaxValue = 1023;
+
+/* button */
+enum btn_states {
+  BTN_PRESSED,
+  PRESSED_TO_RELEASED,
+  BTN_RELEASED,
+  RELEASED_TO_PRESSED
+};
+btn_states button = BTN_RELEASED;
+int buttonState = 0;
+
+void setup() {
+  // declare pins
+  pinMode(DIAL, INPUT);
+  pinMode(BTN_1, INPUT);
+  pinMode(BTN_2, INPUT);
+
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  dialCurrValue = analogRead(DIAL);
 
+  if (dialCurrValue <= dialMaxValue/9) {
+    Serial.println("potentiometer");
+    Serial.println(0);  // box index 0 selected
+  }
+  else if (dialCurrValue > dialMaxValue/9 && dialCurrValue <= dialMaxValue/9*2) {
+    Serial.println("potentiometer");
+    Serial.println(1);  // box index 1 selected
+  }
+  else if (dialCurrValue > dialMaxValue/9*2 && dialCurrValue <= dialMaxValue/9*3) {
+    Serial.println("potentiometer");
+    Serial.println(2);  // box index 2 selected
+  }
+  else if (dialCurrValue > dialMaxValue/9*3 && dialCurrValue <= dialMaxValue/9*4) {
+    Serial.println("potentiometer");
+    Serial.println(3);  // box index 3 selected
+  }
+  else if (dialCurrValue > dialMaxValue/9*4 && dialCurrValue <= dialMaxValue/9*5) {
+    Serial.println("potentiometer");
+    Serial.println(4);  // box index 4 selected
+  }
+  else if (dialCurrValue > dialMaxValue/9*5 && dialCurrValue <= dialMaxValue/9*6) {
+    Serial.println("potentiometer");
+    Serial.println(5);  // box index 5 selected
+  }
+  else if (dialCurrValue > dialMaxValue/9*6 && dialCurrValue <= dialMaxValue/9*7) {
+    Serial.println("potentiometer");
+    Serial.println(6);  // box index 6 selected
+  }
+  else if (dialCurrValue > dialMaxValue/9*7 && dialCurrValue <= dialMaxValue/9*8) {
+    Serial.println("potentiometer");
+    Serial.println(7);  // box index 7 selected
+  }
+  else if (dialCurrValue > dialMaxValue/9*8) {
+    Serial.println("potentiometer");
+    Serial.println(8);  // box index 8 selected
+  }
+
+  pushbutton(BTN_1);
+  pushbutton(BTN_2);
 }
+
+void pushbutton(int btn) {
+  // read the state of the pushbutton value
+  buttonState = digitalRead(btn);
+
+  // determines next state based on the current pushbutton state
+  switch (button) {
+    // if button is pushed, go to transition state, if not, stay in current state
+    case BTN_RELEASED:
+      if (buttonState == HIGH) button = RELEASED_TO_PRESSED;
+      else button = BTN_RELEASED;
+      break;
+    // while in transition state, wait for 50 ms for the pushbutton to become steady
+    // afterwards, send serial msg, then go to the pressed state
+    case RELEASED_TO_PRESSED:
+      delay(50);
+      button = BTN_PRESSED;
+      if (btn == BTN_1) {
+        Serial.println("button");
+        Serial.println(1);  // button 1 pressed
+      }
+      else if (btn == BTN_2) { 
+        Serial.println("button");
+        Serial.println(2);  // button 2 pressed
+      }
+      break;
+    // if button is released, go to transition state, if not, stay in current state
+    case BTN_PRESSED:
+      if (buttonState == LOW) button = PRESSED_TO_RELEASED;
+      else button = BTN_PRESSED;
+      break;
+    // while in transition state, wait for 50 ms for the pushbutton to become steady, then go to the released state
+    case PRESSED_TO_RELEASED:
+      delay(50);
+      button = BTN_RELEASED;
+      break;
+  }
+}
+
--- PhysicalInterface/sketch.js
+++ PhysicalInterface/sketch.js
@@ -17,35 +17,36 @@ class boxObj {
     this.centerX = this.x+this.w/2;
     this.centerY = this.y+this.h/2;
     this.opacity = 0;
-    this.fadeAmount = 0.2;
+    this.fadeAmount = 2;
     this.selected = 0;
-    this.piece = 1;
+    this.assigned = 0;
+    this.piece = 0;
   }
 
   display() {
+    if (this.selected === 0) this.opacity = 0;  // reset opacity for unselected box
     noStroke();
     fill(120, this.opacity);
     rect(this.x, this.y, this.w, this.h);
+    this.fill();
   }
 
   flash() {
+    // don't flash assigned box
+    if (this.assigned === 1) {
+      this.opacity = 0;
+      return;
+    }
+
     this.opacity += this.fadeAmount;
     if (this.opacity <= 0 || this.opacity >= 190) {
       this.fadeAmount = -this.fadeAmount;
     }
-    noStroke();
-    fill(200);
-    textFont('Helvetica', 18);
-    textAlign(CENTER, CENTER);
-    if (this.piece === 1) { 
-      text('× Turn', this.centerX, this.centerY);
-    }
-    else if (this.piece === 2) {
-      text('○ Turn', this.centerX, this.centerY);
-    }
   }
 
-  place() {
+  fill() {
+    if (this.assigned === 0) return;
+    
     noStroke();
     fill(235);
     textFont('Helvetica', 250);
@@ -57,9 +58,52 @@ class boxObj {
       text('○', this.centerX, this.centerY);
     }
   }
+
+  assign() {
+    // can't re-assign
+    if (this.assigned === 1) return;
+
+    if (btnValue === 1) { 
+      this.piece = 1;
+      turn = 2;
+    }
+    else if (btnValue === 2) {
+      this.piece = 2;
+      turn = 1;
+    }
+    this.assigned = 1;
+  }
+
+  reset() {
+    this.opacity = 0;
+    this.selected = 0;
+    this.assigned = 0;
+    this.piece = 0;
+  }
+}
+
+function keyPressed() {
+  if (keyCode === 82) { // R
+    init = 1;
+  }
+}
+
+function restart() {
+  init = 0;
+  turn = 1;
+  for (let i = 0; i < boxes.length; i++) {
+    boxes[i].reset();
+  }
 }
 
 var boxes = [];
+var turn = 1;
+var init = 0;
+var serial;
+var portName = "COM3";
+var currInput;
+var potValue = 0;
+var btnValue = 0;
 
 function setup() {
   createCanvas(600, 600);
@@ -67,10 +111,24 @@ function setup() {
   boxes = [new boxObj(0, 0), new boxObj(width/3, 0), new boxObj(width/3*2, 0), 
     new boxObj(0, height/3), new boxObj(width/3, height/3), new boxObj(width/3*2, height/3),
     new boxObj(0, height/3*2), new boxObj(width/3, height/3*2), new boxObj(width/3*2, height/3*2),];
+
+  serial = new p5.SerialPort();
+
+  //serial.onList(gotList);
+  serial.list();
+
+  //serial.onOpen(gotOpen);
+  serial.openPort(portName);
+
+  serial.onData(gotData);
 }
 
 function draw() {
   background(50);
+
+  if (init === 1) {
+    restart();
+  }
 
   // border
   noStroke();
@@ -82,6 +140,67 @@ function draw() {
 
   // boxes
   for (let i = 0; i < boxes.length; i++) {
+    if (i === potValue) {
+      boxes[i].selected = 1;
+      boxes[i].flash();
+    }
+    else {
+      boxes[i].selected = 0;
+    }
+
     boxes[i].display();
   }
+
+  // turn
+  noStroke();
+  fill(45, 210, 160);
+  textStyle(BOLD);
+  textFont('Courier New', 16);
+  textAlign(CENTER, CENTER);
+  if (turn === 1) { 
+    text('× Turn', 300, 15);
+  }
+  else if (turn === 2) {
+    text('○ Turn', 300, 15);
+  }
+
+  // restart
+  noStroke();
+  fill(45, 210, 160);
+  textStyle(BOLD);
+  textFont('Courier New', 14);
+  textAlign(CENTER, CENTER);
+  text('Press [R] to restart', 300, 585);
 }
+
+// function gotList(ports) {
+//   for (let i = 0; i < ports.length; i++) {
+//     console.log(ports[i]);
+//   }
+// }
+
+// function gotOpen() {
+//   console.log("Port Open!");
+// }
+
+function gotData() {
+  let newData = serial.readLine();
+  if (newData.length <= 0) return;
+  //console.log(newData);
+
+  if (newData === "potentiometer" || newData === "button") {
+    currInput = newData;
+  }
+  else {
+    if (currInput === "potentiometer") {
+      potValue = int(newData);
+    }
+    if (currInput === "button") {
+      btnValue = int(newData);
+      if (btnValue === turn) {
+        boxes[potValue].assign();
+      }
+    }
+  }
+}
+
