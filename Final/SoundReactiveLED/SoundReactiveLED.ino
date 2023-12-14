9b41cbf1b93ede073bc6975cebe863067ad08fb8
Chenyi Wang <cyeeeew@gmail.com>
2023-12-14 16:05:48 -0500

    Modify LED react

--- Final/SoundReactiveLED/SoundReactiveLED.ino
+++ Final/SoundReactiveLED/SoundReactiveLED.ino
@@ -5,7 +5,6 @@ Date: 12/14/22
 
 Sound Reactive Installation
 
-LED Panel Tutorial: https://www.instructables.com/Getting-Started-With-NeoPixle-WS2812-RGB-LED/
 LED Panel Library: https://fastled.io/
 
 */
@@ -18,10 +17,11 @@ FASTLED_USING_NAMESPACE
 #define LED_TYPE    WS2811
 #define COLOR_ORDER GRB
 #define NUM_LEDS    64
+#define WIDTH       8
 CRGB leds[NUM_LEDS];
 
-// #define BRIGHTNESS         30
-int brightness = 0;
+#define BRIGHTNESS         30
+bool rows[WIDTH] = {false};
 
 const int SoundSensor = A0;
 int soundValue = 0; 
@@ -34,7 +34,7 @@ void setup() {
   FastLED.addLeds<LED_TYPE,DATA_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
 
   // set master brightness control
-  // FastLED.setBrightness(BRIGHTNESS);
+  FastLED.setBrightness(BRIGHTNESS);
 
   pinMode(SoundSensor, INPUT);
 
@@ -43,16 +43,110 @@ void setup() {
 
 void loop() {
   
-  soundValue = analogRead(SoundSensor); // 0-1023
-  reversedValue = 1023 - soundValue;    // when the soundsensor is HIGH (no sound detected), the vaule is 1023
+  soundValue = analogRead(SoundSensor); // 0-1023: when the soundsensor is HIGH (no sound detected), the vaule is 1023
 
-  brightness = reversedValue / 30;
+  setRows();
 
-  FastLED.setBrightness(brightness);
-
-  for (int i = 0; i < NUM_LEDS; i++) {
-    leds[i] = CRGB::OrangeRed;
+  for (int i = 0; i < WIDTH; i++) {
+    if (rows[i]) {
+      for( int j = 0; j < WIDTH; j++) { 
+        leds[j+8*i] = CRGB::OrangeRed;
+      }
+    }
+    else {
+      for( int j = 0; j < WIDTH; j++) { 
+        leds[j+8*i] = CRGB::Black;
+      }
+    }
   }
   FastLED.show();
+  
+}
+
+void setRows() {
+  // by testing, when it's lound, sound value is around 70
+  // when it's quiet, sound value is 1023
+  // since there are 8 rows, interval would be approximately 130
+
+  if (soundValue < 1023 && soundValue >= 870) {
+    rows[0] = true;
+    rows[1] = false;
+    rows[2] = false;
+    rows[3] = false;
+    rows[4] = false;
+    rows[5] = false;
+    rows[6] = false;
+    rows[7] = false;
+  }
+  else if (soundValue < 870 && soundValue >= 740) {
+    rows[0] = true;
+    rows[1] = true;
+    rows[2] = false;
+    rows[3] = false;
+    rows[4] = false;
+    rows[5] = false;
+    rows[6] = false;
+    rows[7] = false;
+  }
+  else if (soundValue < 740 && soundValue >= 610) {
+    rows[0] = true;
+    rows[1] = true;
+    rows[2] = true;
+    rows[3] = false;
+    rows[4] = false;
+    rows[5] = false;
+    rows[6] = false;
+    rows[7] = false;
+  }
+  else if (soundValue < 610 && soundValue >= 480) {
+    rows[0] = true;
+    rows[1] = true;
+    rows[2] = true;
+    rows[3] = true;
+    rows[4] = false;
+    rows[5] = false;
+    rows[6] = false;
+    rows[7] = false;
+  }
+  else if (soundValue < 480 && soundValue >= 350) {
+    rows[0] = true;
+    rows[1] = true;
+    rows[2] = true;
+    rows[3] = true;
+    rows[4] = true;
+    rows[5] = false;
+    rows[6] = false;
+    rows[7] = false;
+  }
+  else if (soundValue < 350 && soundValue >= 220) {
+    rows[0] = true;
+    rows[1] = true;
+    rows[2] = true;
+    rows[3] = true;
+    rows[4] = true;
+    rows[5] = true;
+    rows[6] = false;
+    rows[7] = false;
+  }
+  else if (soundValue < 220 && soundValue >= 90) {
+    rows[0] = true;
+    rows[1] = true;
+    rows[2] = true;
+    rows[3] = true;
+    rows[4] = true;
+    rows[5] = true;
+    rows[6] = true;
+    rows[7] = false;
+  }
+  else if (soundValue < 90) {
+    rows[0] = true;
+    rows[1] = true;
+    rows[2] = true;
+    rows[3] = true;
+    rows[4] = true;
+    rows[5] = true;
+    rows[6] = true;
+    rows[7] = true;
+  }
   
 }
